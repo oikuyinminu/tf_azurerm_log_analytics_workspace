f5b839ad6511d19bdb16eff8dcb82f4f8309eda3
Banji IKUYINMINU <i.banji@hotmail.com>
2022-04-18 08:43:19 +1000

    adjustment to link services

--- variables.tf
+++ variables.tf
@@ -72,6 +72,6 @@ variable "linked_service_resource" {
 
  validation {
    condition = can(regex("^(?i:/subscriptions/.+?/resourceGroups/.+?/providers/Microsoft.Automation/automationAccounts/.+|null))$", format("%v", var.linked_service_resource.id)))
-   error_message = "Automation Account ID is invalid"
+   error_message = "Automation Account ID is invalid."
  }
 }
